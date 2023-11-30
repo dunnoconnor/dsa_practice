f886d3ac6392a9af45a50e37d0c100a0cd204656
Salem Boyland <salemb@google.com>
2023-11-30 14:16:56 -0600

    summary of problem

---
+++ recursion.py
@@ -0,0 +1,16 @@
+#Assignment
+#Write a function, sumNums, that takes a number, num, as an argument and returns the sum of 
+#all the numbers between 1 and num. You can assume that num will be greater than 1. Use recursion.
+# example: 
+#sumNums(3); // => 6 (3 + 2 + 1)
+
+
+# 1.) Summary of the problem in my own words
+'''
+Write a recursive function, sumNums, that takes a positive integer num as an argument and 
+returns the sum of all the positive integers from 1 to num.
+'''
+# 2.) Write test cases for example inputs and outputs
+# 3.) Pseudocode your plan in comments
+# 4.) Commit your code as you go until you solution is complete
+
