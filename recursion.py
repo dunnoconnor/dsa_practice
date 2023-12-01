d31b85635f4f6b52532d44be604be4bbe78b3a89
Salem Boyland <salemb@google.com>
2023-12-01 09:23:46 -0600

    recursion function solution and psuedocode

--- recursion.py
+++ recursion.py
@@ -11,6 +11,26 @@ Write a recursive function, sumNums, that takes a positive integer num as an arg
 returns the sum of all the positive integers from 1 to num.
 '''
 # 2.) Write test cases for example inputs and outputs
+
+sumNums(5)  # Output: 15
+
 # 3.) Pseudocode your plan in comments
+
+# sumNums() this is the function 
+
+# sumNums(2) input is 2, which will recursively 
+
+# actually, I should set a variable as 'nums', which will allow me to change the input,
+#  but the function will still work for any numbered input.
+
+
 # 4.) Commit your code as you go until you solution is complete
 
+num = 25
+
+def sumNums(num):
+    if num == 1:
+        print(1)
+    else:
+      num == 1:
+        print(num + sumNums(num - 1))
